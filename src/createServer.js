3df1e9c6ccf0b502ad5f3519371d3ac369d420da
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-03-16 21:29:47 +0200

    Solution2

--- src/createServer.js
+++ src/createServer.js
@@ -3,12 +3,25 @@
 const http = require('node:http');
 const fsp = require('fs/promises');
 const url = require('url');
+const path = require('node:path');
 
 function createServer() {
   const server = http.createServer(async (req, res) => {
     const normalizedURL = new url.URL(req.url, `http://${req.headers.host}`);
     const { pathname } = normalizedURL;
-    const filePath = pathname.slice(6).trim() || 'index.html';
+    const publicDir = path.resolve(__dirname, 'public');
+
+    const filePath = path.join(
+      publicDir,
+      pathname.slice(6).trim() || 'index.html',
+    );
+
+    if (!filePath.startsWith(publicDir)) {
+      res.statusCode = 400;
+      res.end('Traversal path');
+
+      return;
+    }
 
     if (!pathname.startsWith('/file/')) {
       res.statusCode = 200;
@@ -27,7 +40,7 @@ function createServer() {
     }
 
     try {
-      const file = await fsp.readFile(`./public/${filePath}`, 'utf-8');
+      const file = await fsp.readFile(filePath, 'utf-8');
 
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
